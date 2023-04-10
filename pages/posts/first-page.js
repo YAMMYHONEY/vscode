997d34d4494b54d3a4ceaa32703c323ac84e7850
YAMMYHONEY <swpark2125@naver.com>
2023-04-11 04:00:00 +0900

    I upload it to submit assignment.

---
+++ pages/posts/first-page.js
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+export default function FirstPage() {
+    return (
+        <div>
+          <h3>☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆☆★</h3>
+          <h2>Hello! Nice to meet you.  My name is Park Sang Won!</h2>
+          <h2>I'm from South Korea.  My age is 21.  I'm Hanyang University Information System student.</h2>
+          <h2>My hobby is reading a book. and My favorite is Netflix. Thank you.</h2>
+          <h3>☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆☆★☆★☆★☆★☆★☆★☆★</h3>
+            <Link href="/">Back to home</Link> //GO TO "/"(MAIN PAGE)
+        </div>
+    )
+}
