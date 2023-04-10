7306b433f3a6f3e3e9c8ae645103904602c9e1c5
YAMMYHONEY <swpark2125@naver.com>
2023-04-11 04:17:14 +0900

    Edit

--- pages/posts/first-page.js
+++ pages/posts/first-page.js
@@ -5,7 +5,8 @@ export default function FirstPage() {
           <h3>☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆☆★</h3>
           <h2>Hello! Nice to meet you.  My name is Park Sang Won!</h2>
           <h2>I'm from South Korea.  My age is 21.  I'm Hanyang University Information System student.</h2>
-          <h2>My hobby is reading a book. and My favorite is Netflix. Thank you.</h2>
+          <h2>My hobby is reading a book. and My favorite is Netflix.</h2>
+          <h2>I like my university. Thank you.</h2>
           <h3>☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆☆★☆★☆★☆★☆★☆★☆★</h3>
             <Link href="/">Back to home</Link> //GO TO "/"(MAIN PAGE)
         </div>
